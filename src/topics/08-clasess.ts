a34120d2b0675174b607e7b6c84e6b47da99ef7b
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-30 23:47:50 -0500

    feat: composicion de clases

--- src/topics/08-clasess.ts
+++ src/topics/08-clasess.ts
@@ -2,7 +2,10 @@ export class Person {
     constructor(
         public name: string, 
         private address: string = 'No Address'
-    ){}
+    ){
+        this.name = name;
+        this.address = address;
+    }
 }
 
 export class Hero extends Person {
@@ -15,9 +18,23 @@ export class Hero extends Person {
     }
 }
 
+export class Hero2 {
+    constructor(
+        public person: Person,
+        public alerEgo: string,
+        public age: number,
+        public realName: string
+    ){
+        this.person = person
+    }
+}
+
 const ricardo = new Person('Ricardo', 'Jesus Maria');
 const rosa = new Person('Rosa', 'La Perla');
 const ironMan = new Hero('Iron Man', 45, 'Tony Stark');
+const person = new Person('Tony Stark', 'Malibu NYC');
+const ironMan2 = new Hero2(person, 'Iron Man', 45, 'Tony Stark');
 console.log(ricardo);
 console.log(rosa);
 console.log(ironMan);
+console.log(ironMan2);
