eb5b121ebbf4c0ef3aed491a1e59b10ce05bd342
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-30 23:40:32 -0500

    feat: extender clases en TS

--- src/topics/08-clasess.ts
+++ src/topics/08-clasess.ts
@@ -1,11 +1,23 @@
 export class Person {
     constructor(
         public name: string, 
-        public address: string
+        private address: string = 'No Address'
     ){}
+}
+
+export class Hero extends Person {
+    constructor(
+        public alerEgo: string,
+        public age: number,
+        public realName: string
+    ){
+        super(realName, 'Malibu');
+    }
 }
 
 const ricardo = new Person('Ricardo', 'Jesus Maria');
 const rosa = new Person('Rosa', 'La Perla');
+const ironMan = new Hero('Iron Man', 45, 'Tony Stark');
 console.log(ricardo);
 console.log(rosa);
+console.log(ironMan);
